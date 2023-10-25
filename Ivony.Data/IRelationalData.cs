b0345c5ee36a5f1f25e80972565e7a148fb7dc23
Ivony <ivony@live.com>
2023-10-25 14:39:11 +0800

    添加数据查询结果的抽象

---
+++ Ivony.Data/IRelationalData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Ivony.Data
+{
+
+  public interface IRelationalData : IReadOnlyList<IRelationalDataRecord>
+  {
+
+  }
+
+  public interface IRelationalDataRecord
+  {
+
+    T FieldValue<T>( string name );
+
+  }
+
+  public interface IRelationalDataSchema : IReadOnlyList<IRelationalDataFieldMapping>
+  {
+
+
+
+  }
+
+  public interface IRelationalDataFieldMapping
+  {
+
+    string Name { get; }
+
+    Type DataType { get; }
+
+    System.Data.DbType DbType { get; }
+
+  }
+}
